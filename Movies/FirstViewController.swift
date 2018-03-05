63b62127fc076c494b5657ff2ff08cdca42018b6
never <never@nevers-iMac.local>
2018-03-05 08:02:37 -0800

    Initial Commit

---
+++ Movies/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Movies
+//
+//  Created by never on 3/5/2561 BE.
+//  Copyright © 2561 never. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
