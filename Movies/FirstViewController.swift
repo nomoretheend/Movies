9054065ce62957536a350ac00a383bc443d4f472
never <never@nevers-iMac.local>
2018-03-10 07:52:14 -0800

    read json

--- Movies/FirstViewController.swift
+++ Movies/FirstViewController.swift
@@ -11,36 +11,76 @@ import Kingfisher
 
 class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    var catImage = "https://images.unsplash.com/photo-1496857239036-1fb137683000?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&s=6adcf066111fa0ee8d5f0b906580bf00"
+    let movieURL = "https://www.majorcineplex.com/apis/get_movie_avaiable"
+    
+    var moviesList = [Movie]()
     
     @IBOutlet weak var movieTable: UITableView!
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath) as! MovieTableViewCell
-        
-        let resource = ImageResource(downloadURL: URL(string: catImage)!, cacheKey: catImage)
-        cell.movieImage.kf.setImage(with: resource)
-        //cell.movieImage.image = UIImage(named: "cat")
-        cell.movieTitle!.text = "randommm"
-        
-        return cell
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("first")
+        loadJson()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return moviesList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieCell", for: indexPath) as! MovieTableViewCell
+        
+        let imagePath = "https://www.majorcineplex.com"+moviesList[indexPath.row].poster_ori
+        let resource = ImageResource(downloadURL: URL(string: imagePath)!, cacheKey: imagePath)
+        cell.movieImage.kf.setImage(with: resource)
+        cell.movieTitle!.text = moviesList[indexPath.row].title_en
+        
+        return cell
+    }
+    
+    struct ResponseData: Decodable {
+        var movies: [Movie]
+    }
+    
+    struct Movie: Decodable {
+        var id: Int
+        var title_en: String
+        var synopsis_en: String
+        var poster_ori: String
+    }
+    
+    func loadJson(){
+        guard let url = URL(string: movieURL) else {return}
+        
+        URLSession.shared.dataTask(with: url) { (data, res, err) in
+            
+            guard let data = data else {return}
+            
+            do{
+                let jsonData = try JSONDecoder().decode(ResponseData.self, from: data)
+                print("loaded")
+                self.moviesList = jsonData.movies
+                print(self.moviesList.count)
+                
+                DispatchQueue.main.async {
+                    self.movieTable.reloadData()
+                }
+                
+            } catch {
+                print("error")
+            }
+            
+            
+            }.resume()
+    }
+    
+    
+    
 }
 
